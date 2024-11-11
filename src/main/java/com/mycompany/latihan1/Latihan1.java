f9fbb1b2fb18ba0490dd6d1e263cd06d78a3061e
wahsur <ngurahsurya736@gmail.com>
2024-11-11 13:07:22 +0800

    tes

---
+++ src/main/java/com/mycompany/latihan1/Latihan1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author I Gusti Ngurah Surya
+ */
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
